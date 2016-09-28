990be90686569b5f47f2bf71aaa02054eb590cdf
bex3 <Rebeccam.bearden@gmail>
2016-09-28 08:56:53 -0400

    closer to fib

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -7,15 +7,14 @@ public class Fibonacci {
     public static void main(String[] args) {
         System.out.println("Please enter a number for the ");
         Scanner inputScanner = new Scanner(System.in);
-        long fibSequence = Integer.valueOf(inputScanner.nextLine());
+        long fibSequenceNum = Long.valueOf(inputScanner.nextLine());
+        long fibReturn = getFib(fibSequenceNum);
+    }
 
+    public static long getFib(long fibSequenceNum){
+        if (fibSequenceNum == 0 || fibSequenceNum == 1){
+            return fibSequenceNum;
+        }
+        return getFib(fibSequenceNum -1) + getFib(fibSequenceNum -2);
     }
 }
-//    public static long getFib(long fibSequence){
-//        if (fibSequence == 0 || fibSequence == 1){
-//            return fibSequenceNum;
-//        }
-//        return getFib(fibSequenceNum -1) + getFib(fibSeqNum -2);
-//    }
-////ummmmmmm
-//}
