8b20a80a304c3dbab572cfec2254d589fe67ba2c
bex3 <Rebeccam.bearden@gmail>
2016-09-27 21:33:53 -0400

    selection works - fib still doesn't

--- src/SelectionSorter.java
+++ src/SelectionSorter.java
@@ -1,18 +1,29 @@
+import com.sun.xml.internal.rngom.parse.host.Base;
+
 /**
  * Created by bearden-tellez on 9/27/16.
  */
 public class SelectionSorter extends BaseSorter {
-    @Override
-    public int[] sort(int[] arrayNeedingSorting) {
-
-        for(int counter = 0; counter < arrayNeedingSorting.length; counter ++){
-
-        //inner loop end
-        }
-        return arrayNeedingSorting;
-        //outer loop end
+    private void swap(int index, int secondIndex, int[] intArray) {
+        int temp = intArray[index];
+        intArray[index] = intArray[secondIndex];
+        intArray[secondIndex] = temp;
     }
 
+    @Override
+    public int[] sort(int[] arrayNeedingSorting) {
+        for (int index = 0; index < arrayNeedingSorting.length - 1; index++) {
+            int smallestPosition = index;
+
+            for (int secondIndex = index + 1; secondIndex <  (arrayNeedingSorting.length); secondIndex++) {
+                if (arrayNeedingSorting[secondIndex] <  (arrayNeedingSorting[smallestPosition])) {
+                    smallestPosition = secondIndex;
+                }
+            }
+            swap(index, smallestPosition, arrayNeedingSorting);
+        }
+        return arrayNeedingSorting;
+    }
 }
 
 
