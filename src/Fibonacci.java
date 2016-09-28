66f67cc4f4020d5982c1ec0ee82187797620e7e6
bex3 <Rebeccam.bearden@gmail>
2016-09-28 08:51:23 -0400

    no fib still

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 /**
  * Created by bearden-tellez on 9/27/16.
  */
 public class Fibonacci {
     public static void main(String[] args) {
         System.out.println("Please enter a number for the ");
-
+        Scanner inputScanner = new Scanner(System.in);
+        long fibSequence = Integer.valueOf(inputScanner.nextLine());
 
     }
 }
